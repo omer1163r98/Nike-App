82e7115f9f4546af3d36f7abb4db7b8906f3b3c5
Omer Abdul <yourname@example.com>
2024-03-07 00:15:31 -0500

    added react router to log in page

--- src/App.jsx
+++ src/App.jsx
@@ -16,34 +16,37 @@ const App =  () => {
     <>
     <Routes>
       <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/" element = { (
+         <main className='relative'>
+         <Nav></Nav>
+         <section className='xl:padding-l wide:padding-r padding-b '>
+         <Hero></Hero>
+         </section>
+         <section className='padding'>
+           <PopularProducts></PopularProducts>
+         </section>
+         <section className='padding'>
+           <SuperQuality></SuperQuality>
+         </section>
+         <section className='padding'>
+           <Services></Services>
+         </section>
+         <section className='padding'>
+           <SpecialOffer></SpecialOffer>
+         </section>
+         <section className='bg-pale-blue padding'>
+           <Reviews></Reviews>
+         </section>
+         <section className='padding-x sm:py-32 py-16 w-full'>
+           <Subscribe></Subscribe>
+         </section>
+         <section className='bg-black padding-x padding-t pb-8'>
+           <Footer></Footer>
+         </section>
+       </main>
+      )}></Route>
     </Routes>
-    <main className='relative'>
-      <Nav></Nav>
-      <section className='xl:padding-l wide:padding-r padding-b '>
-      <Hero></Hero>
-      </section>
-      <section className='padding'>
-        <PopularProducts></PopularProducts>
-      </section>
-      <section className='padding'>
-        <SuperQuality></SuperQuality>
-      </section>
-      <section className='padding'>
-        <Services></Services>
-      </section>
-      <section className='padding'>
-        <SpecialOffer></SpecialOffer>
-      </section>
-      <section className='bg-pale-blue padding'>
-        <Reviews></Reviews>
-      </section>
-      <section className='padding-x sm:py-32 py-16 w-full'>
-        <Subscribe></Subscribe>
-      </section>
-      <section className='bg-black padding-x padding-t pb-8'>
-        <Footer></Footer>
-      </section>
-    </main>
+   
     </>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Button from './Button'
 import { arrowRight } from '../../assets/icons'
 import { close } from '../../assets/icons'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const navigate = useNavigate()
   return (
+    <section className='flex justify-center mt-[10%]'>
     <div className='flex flex-col bg-white w-[500px] h-[500px] absolute z-20 items-center rounded-2xl shadow-xl'>
       <h2 className='text-2xl font-bold text-coral-red font-palanquin mt-16 mb-4'>Log in</h2>
       <form className='flex flex-col'>
@@ -18,9 +21,10 @@ const Login = () => {
         <p>Forgot Password?</p>
         <p>Sign Up</p>
       </div>
-      <img width={20} src={close} className='absolute left-[92%] top-[5%] cursor-pointer'/>
+      <img onClick={() => navigate('/')} width={20} src={close} className='absolute left-[92%] top-[5%] cursor-pointer'/>
 
     </div>
+    </section>
   )
 }
 
